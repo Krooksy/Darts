c245c64391d7d4560c7103f02c9429d4321a3c34
Jordan <jordan.willis10@gmail.com>
2020-12-22 18:12:42 +0000

    Added overtime and first thrower function

--- Darts.js
+++ Darts.js
@@ -3,7 +3,6 @@ let player1 = {
     name: 'Jordan',
     winner: false,
     scoreRemaining: 501,
-    roundsWon: 0,
     turnsTaken: 0,
     deciderShot: 0,
 };
@@ -12,9 +11,13 @@ let player2 = {
     name: 'Katie',
     winner: false,
     scoreRemaining: 501,
-    roundsWon: 0,
     turnsTaken: 0,
     deciderShot: 0,
+};
+
+let roundScores = {
+    jordan: 0,
+    katie: 0,
 };
 
 // Possible scores with a single dart, each time the function is called, a random value from the array is selected.
@@ -31,7 +34,7 @@ const randomMultiplier = () => {
     return multiplierChance[randNum];
 };
 
-// Words used later on in logging to console what the dart scored.
+// Words used later on in logging to console what the dart's multiplier was.
 const multiplierWords = {
     1: 'Single',
     2: 'Double',
@@ -42,18 +45,56 @@ const multiplierWords = {
 const decideFirstThrower = () => {
     player1.deciderShot = randomBoardScore() * randomMultiplier();
     player2.deciderShot = randomBoardScore() * randomMultiplier();
+
+    // Changes the name of player one based on the score of their throws.
     if (player1.deciderShot < player2.deciderShot) {
         player1.name = 'Katie';
         player2.name = 'Jordan';
-        console.log(`${player1.name} scored a higher score of ${player2.deciderShot} and will throw first. \n`);
+        console.log(`${player1.name} scored a higher score of ${player2.deciderShot} against ${player2.name}'s ${player1.deciderShot} and will throw first. \n`);
     } else {
-        console.log(`${player1.name} scored a higher score of ${player1.deciderShot} and will throw first. \n`);
+        console.log(`${player1.name} scored a higher score of ${player1.deciderShot} aginst ${player2.name}'s ${player2.deciderShot} and will throw first. \n`);
     }
+
+    // Reset values to zero for the next loop.
+    player1.deciderShot = player2.deciderShot = 0;
+};
+
+// This function decides a winner if the game goes to a tie.
+const overtimeWinner = (player) => {
+    let threeDartTotal = 0;
+    // the loop starts at 1 to make it easier to log which dart number is being thrown
+    for (let i = 1; i < 4; i++) {
+        let remainder = player.scoreRemaining - threeDartTotal;
+
+        // Assigns score to a dart with a potential multiplier.
+        const boardScore = randomBoardScore();
+        let boardMulti = randomMultiplier();
+
+        // Sets Multiplier to 1 if either of the centre points are scored - these can't have doubles or triples.
+        if (boardScore == 25 || boardScore == 50) {
+            boardMulti = 1;
+        }
+
+        // Calculates the single dart scores and accumalates them
+        const singleDart = boardScore * boardMulti;
+        threeDartTotal += singleDart;
+
+        // Logs the score of darts to the console after each throw. Uses the multiplierWords object to access the multplier words as needed
+        if (boardScore == 50) {
+            console.log(`Dart ${i} scored ${singleDart} with a BULLSEYE!`);
+        } else if (boardScore == 0) {
+            console.log(`Dart ${i} MISSED!`);
+        } else {
+            console.log(`Dart ${i} scored ${singleDart} with a ${multiplierWords[boardMulti]} ${boardScore}`);
+        }
+    }
+    console.log(`${player.name} scored ${threeDartTotal}! \n`);
+    return threeDartTotal;
 };
 
 // This function similutes the chance of throwing a double for the win.
 const doubleToWin = (player, dtwNum) => {
-    
+
     // Gives a 50/50 chance of scoring the double  
     const winProbabilityArray = [0, 0, 1, 1];
     const winProbChance = Math.floor(Math.random() * winProbabilityArray.length);
@@ -74,13 +115,12 @@ const throwDarts = (player) => {
     console.log(`${player.name} to throw. ${player.scoreRemaining} needed.`);
     let threeDartTotal = 0;
     let previousTotal = player.scoreRemaining;
-    
-    // Loops through 3 times and accumulates the dart scores
-    for (let i = 1; i < 4; i++) {     
+
+    // Loops through 3 times and accumulates the dart scores and the loop starts at 1 to make it easier to log which dart number is being thrown
+    for (let i = 1; i < 4; i++) {
         let remainder = player.scoreRemaining - threeDartTotal;
-        
+
         if (remainder <= 40 && remainder > 1 && remainder % 2 == 0) {
-            // console.log(`${player.name} requires a double to win... \n`);
             doubleToWin(player, remainder);
             break;
         }
@@ -93,9 +133,9 @@ const throwDarts = (player) => {
         if (boardScore == 25 || boardScore == 50) {
             boardMulti = 1;
         }
-        
+
         // Calculates the single dart scores and accumalates them
-        const singleDart = boardScore * boardMulti;   
+        const singleDart = boardScore * boardMulti;
         threeDartTotal += singleDart;
 
         // Logs the score of darts to the console after each throw. Uses the multiplierWords object to access the multplier words as needed
@@ -104,7 +144,7 @@ const throwDarts = (player) => {
         } else if (boardScore == 0) {
             console.log(`Dart ${i} MISSED!`);
         } else {
-            console.log(`Dart ${i} scored ${singleDart} with a ${multiplierWords[boardMulti]} ${boardScore}`);    
+            console.log(`Dart ${i} scored ${singleDart} with a ${multiplierWords[boardMulti]} ${boardScore}`);
         }
     }
 
@@ -117,7 +157,7 @@ const throwDarts = (player) => {
     }
 
     // Subtracts the accumalation of the three darts from the target score and logs to console or declares player bust and resets their score to the previous total.
-    if(player.scoreRemaining - threeDartTotal <= 1) {
+    if (player.scoreRemaining - threeDartTotal <= 1) {
         player.scoreRemaining = previousTotal;
         console.log(`BUST! ${player.name} has ${player.scoreRemaining} remaining. \n`);
         return player.scoreRemaining;
@@ -129,11 +169,9 @@ const throwDarts = (player) => {
 };
 
 // Decide who throws first with 1 dart each, highest score throws first.
-
-
-// while neither player has been designated the winner, continue playing until the score is within the 'double to win' window.
 decideFirstThrower();
 
+// while neither player has been designated the winner, continue playing until the score is within the 'double to win' window.
 while (player1.winner == false && player2.winner == false) {
     player1.scoreRemaining = throwDarts(player1);
 
@@ -145,22 +183,29 @@ while (player1.winner == false && player2.winner == false) {
         break;
     }
 
-    
+    // Check winning scenario and display the coressponding result.
+
+    // Overtime
     if (player1.winner == true && player2.winner == true && player1.turnsTaken == player2.turnsTaken) {
-        console.log('The game has ended in a draw.');
+        console.log('Tie game! Overtime coming up... \n');
+        const winner = overtimeWinner(player1) > overtimeWinner(player2) ? player1 : player2;
+        winner.roundsWon++;
+        console.log(`The winner is ${winner.name}!`);
         break;
+
     } else if (player1.winner || player2.winner == true) {
         const winner = player1.scoreRemaining < player2.scoreRemaining ? player1 : player2;
         console.log(`${winner.name} has won!`);
+        roundScores[winner.name.toLowerCase()]++;
         break;
     }
-    
+
     if (player1.winner == false && player2.winner == false && player1.turnsTaken > 20 && player2.turnsTaken > 20) {
         const suddenDeathWinner = player1.scoreRemaining < player2.scoreRemaining ? player1 : player2;
-        suddenDeathWinner.winner = true;
+        suddenDeathWinner.roundsWon++;
         console.log(`The game has surpassed 20 turns! ${suddenDeathWinner.name} is the winner with the lower score of ${suddenDeathWinner.scoreRemaining}`);
         break;
     }
 }
-
+console.log(roundScores.jordan, roundScores.katie);
 console.log('Game over.');
